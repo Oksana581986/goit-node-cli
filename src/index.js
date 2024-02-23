745c2061ace2ed0f9ea28c566b157d0446fc3fac
Oksana581986 <oksanaotchych@gmail.com>
2024-02-23 16:41:17 +0200

    nodeactive

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import { program } from "commander";
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(await listContacts());
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      if (!contact) {
+        throw Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deleletedContact = await removeContact(id);
+      if (!deleletedContact) {
+        throw Error(`Contact with id=${id} not found`);
+      }
+      console.log(deleletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
